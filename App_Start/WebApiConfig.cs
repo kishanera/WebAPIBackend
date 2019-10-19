e73f34964bbdf5674cd4725615870b158d4b15b3
kishanera <kishanera@gmail.com>
2019-10-19 08:05:48 +0530

    first cut

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApiDemo
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.EnableCors();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // formatters
+            config.Formatters.Add(new BrowserJsonFormatter());
+        }
+    }
+}
